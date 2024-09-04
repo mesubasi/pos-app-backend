0988e8a7eb54ec35c9b33268803631a5a4674de7
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-09-04 23:06:52 +0300

    auth.js edirts

--- routes/auth.js
+++ routes/auth.js
@@ -1,58 +1,14 @@
-const User = require("../models/User");
-const router = require("express").Router();
-const bcrypt = require("bcryptjs");
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
 
-//POST Register
-router.post("/register", async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-    const existingUser = await User.findOne({ email });
-
-    if (existingUser) {
-      return res.status(403).json({ error: "Email already in use!" });
-    }
-
-    const newUser = new User({
-      username,
-      email,
-      password: hashedPassword,
-    });
-    await newUser.save();
-    res.status(200).json("A new user created successfully.");
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-//POST Login
-router.post("/login", async (req, res) => {
-  try {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) {
-      return res.status(404).json({ error: "User not found!" });
-    }
-
-    const validPassword = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
-
-    if (!validPassword) {
-      return res.status(403).json({ error: "Invalid Password!" });
-    }
-
-    res.status(200).json({
-      user: {
-        id: user._id,
-        email: user.email,
-        username: user.username,
-      },
-    });
-  } catch (err) {
-    res.status(500).json({ error: "Login failed", details: err.message });
-  }
-});
-
-module.exports = router;
+router.post(
+  "/login",
+  userController.registerUser,
+  userController.loginUser,
+  userController.getLoggedInUser,
+  userController.getUsers,
+  userController.getUserById,
+  userController.updateUser,
+  userController.deleteUser
+);
