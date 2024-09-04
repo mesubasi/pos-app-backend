cb71c1e5acd77459a231bf48d11ba09727bb9dcc
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-09-04 23:10:55 +0300

    auth.js Edits

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,25 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-router.post(
-  "/login",
-  userController.registerUser,
-  userController.loginUser,
-  userController.getLoggedInUser,
-  userController.getUsers,
-  userController.getUserById,
-  userController.updateUser,
-  userController.deleteUser
-);
+// Kullanıcı kaydı
+router.post("/register", userController.registerUser);
+
+// Kullanıcı girişi
+router.post("/login", userController.loginUser);
+
+// Kimliği doğrulanmış kullanıcının bilgilerini al
+router.get("/me", userController.getLoggedInUser);
+
+// Admin: Tüm kullanıcıları al
+router.get("/", userController.getUsers);
+
+// Admin: ID'ye göre kullanıcı al
+router.get("/:id", userController.getUserById);
+
+// Kullanıcı veya admin: Kullanıcıyı güncelle
+router.put("/:id", userController.updateUser);
+
+// Admin: Kullanıcıyı sil
+router.delete("/:id", userController.deleteUser);
+
+module.exports = router;
