f5002976d65e30178f2508e72e212c4be84b551e
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-09-04 22:56:57 +0300

    Changed mongoose connect

--- server.js
+++ server.js
@@ -20,7 +20,10 @@ const PORT = process.env.PORT || 5000;
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to mongoDB");
   } catch (err) {
     throw err;
