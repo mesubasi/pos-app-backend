2b8265652e172730994286b1921a75411d4ebbb1
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-09-05 22:34:34 +0300

    handleLogin Added

---
+++ controllers/loginController.js
@@ -0,0 +1,29 @@
+//POST Login
+
+const handleLogin = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).json({ error: "User not found!" });
+    }
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!validPassword) {
+      return res.status(403).json({ error: "Invalid Password!" });
+    }
+
+    res.status(200).json({
+      user: {
+        id: user._id,
+        email: user.email,
+        username: user.username,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ error: "Login failed", details: err.message });
+  }
+};
--- routes/auth.js
+++ routes/auth.js
@@ -26,33 +26,4 @@ router.post("/register", async (req, res) => {
   }
 });
 
-//POST Login
-router.post("/login", async (req, res) => {
-  try {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) {
-      return res.status(404).json({ error: "User not found!" });
-    }
-
-    const validPassword = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
-
-    if (!validPassword) {
-      return res.status(403).json({ error: "Invalid Password!" });
-    }
-
-    res.status(200).json({
-      user: {
-        id: user._id,
-        email: user.email,
-        username: user.username,
-      },
-    });
-  } catch (err) {
-    res.status(500).json({ error: "Login failed", details: err.message });
-  }
-});
-
 module.exports = router;
