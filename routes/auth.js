0168eb661e8285a8899b80da901315d31376c9e4
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-09-05 22:41:38 +0300

    API Edits

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const loginController = require("../controllers/loginController");
 const registerController = require("../controllers/registerController");
 
-router.post("/", loginController.handleLogin);
-router.post("/", registerController.handleRegister);
+router.post("/login", loginController.handleLogin);
+router.post("/register", registerController.handleRegister);
 
 module.exports = router;
