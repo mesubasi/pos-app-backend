c02df7f22c3afba25a026ec25d95f1037be26eeb
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-09-05 22:35:37 +0300

    handleRegister Added

---
+++ controllers/registerController.js
@@ -0,0 +1,26 @@
+//POST Register
+
+const handleRegister = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(403).json({ error: "Email already in use!" });
+    }
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    res.status(200).json("A new user created successfully.");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+module.exports = handleRegister;
--- routes/auth.js
+++ routes/auth.js
@@ -2,28 +2,4 @@ const User = require("../models/User");
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 
-//POST Register
-router.post("/register", async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-    const existingUser = await User.findOne({ email });
-
-    if (existingUser) {
-      return res.status(403).json({ error: "Email already in use!" });
-    }
-
-    const newUser = new User({
-      username,
-      email,
-      password: hashedPassword,
-    });
-    await newUser.save();
-    res.status(200).json("A new user created successfully.");
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 module.exports = router;
