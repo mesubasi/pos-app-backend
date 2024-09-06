1784f825e41dfc0731436a0f8f233ce1de49e448
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-09-06 21:37:46 +0300

    refreshTokenController and logoutController Added

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 const loginController = require("../controllers/loginController");
 const registerController = require("../controllers/registerController");
+const refreshTokenController = require("../controllers/refreshTokenController");
+const logoutController = require("../controllers/logoutController");
 
 router.post("/login", loginController.handleLogin);
 router.post("/register", registerController.handleRegister);
+router.post("/refresh", refreshTokenController.handleRefreshToken);
+router.post("/logout", logoutController.handleLogout);
 
 module.exports = router;
